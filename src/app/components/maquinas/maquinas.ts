853ac4c167e7b46efd73403a8ca7b46fb8b8f38d
aaron <gu24ia0022@globaluniversity.edu.mx>
2026-07-02 19:59:24 -0600

    feat: generate crud components and core services

---
+++ src/app/components/maquinas/maquinas.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maquinas',
+  imports: [],
+  templateUrl: './maquinas.html',
+  styleUrl: './maquinas.css',
+})
+export class Maquinas {}
